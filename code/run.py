2ee44b89a5e5da01a5f8cc4491cd336b0add4950
JulienB <julienbraine@yahoo.fr>
2024-10-21 18:24:19 +0200

    update

---
+++ code/run.py
@@ -0,0 +1,17 @@
+import papermill, subprocess
+import sys, yaml, shutil
+from pathlib import Path
+import helper
+
+param_path = Path(sys.argv[1])
+params = yaml.safe_load(param_path.open("r"))
+if "run_summary_folder" in params:
+    variables =  params["variables"] if "variables" in params else {}
+    summary_folder = Path(helper.replace_vals(params["run_summary_folder"], variables))
+else:
+    summary_folder = Path(".run.tmp")
+if summary_folder.exists():
+    shutil.rmtree(summary_folder)
+summary_folder.mkdir(parents=True)
+papermill.execute_notebook("run.ipynb", summary_folder/"run.ipynb", parameters=dict(param_path=str(param_path)))
+subprocess.run(f'jupyter nbconvert --to html {summary_folder/"run.ipynb"}', shell=True, check=True)
