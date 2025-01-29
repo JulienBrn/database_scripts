a8a88da634f83d364ef104808854f5b00e77f0df
JulienB <julienbraine@yahoo.fr>
2025-01-29 15:59:03 +0100

    update

--- code/run.py
+++ code/run.py
@@ -241,7 +241,7 @@ def execute_runs(i, cell):
                 display_str+=final_display_df.iloc[0:dcut].groupby("dyn_status").size().reset_index().to_markdown()+"\n"
             display_str += final_display_df.iloc[dcut:fcut, :].to_markdown()
             if len(display_df.index) - fcut > 0:
-                display_str +=f"\n{len(display_df.index) - fcut} more...\n"
+                display_str +=f"\n{len(display_df.index) - fcut-1} more...\n"
                 display_str+=final_display_df.iloc[fcut+1:].groupby("dyn_status").size().reset_index().to_markdown()+"\n"
         else:
             display_str = final_display_df.iloc[dcut:fcut, :].to_markdown()
